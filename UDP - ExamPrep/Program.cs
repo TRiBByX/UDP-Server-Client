8bf3e2bbbc931bf37f8c7fca054b77f983023b42
TRiBB <TRiBB@WORK>
2017-01-09 03:51:48 +0100

    Made a functioning udp server and client

--- UDP - ExamPrep/Program.cs
+++ UDP - ExamPrep/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,39 @@ namespace UDP___ExamPrep
     {
         static void Main(string[] args)
         {
+            //Send to IP
+            var server = new UdpClient(8989);
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, 8989);
+
+            try
+            {
+                Console.WriteLine("Server is blocked");
+                while (true)
+                {
+                    Byte[] receiveBytes = server.Receive(ref endPoint);
+                    //server is now active
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    string[] data = receivedData.Split(' ');
+                    string clientName = data[0];
+                    string text = data[1] + data[2];
+
+                    Console.WriteLine(receivedData);
+
+                    string sendData = $"Server: {text.ToUpper()}";
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(sendData);
+
+                    server.Send(sendBytes, sendBytes.Length, endPoint);
+
+                    Console.WriteLine($"This Message was sent from {endPoint.Address.ToString()} on their port number {endPoint.Port.ToString()}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
---
+++ UDP Client - ExamPrep/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UDP_Client___ExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            UdpClient client = new UdpClient("127.0.0.1", 8989);
+
+            IPEndPoint endPoint = new IPEndPoint(ip, 8989);
+
+            Console.WriteLine("State name: ");
+            string name = Console.ReadLine();
+
+            for (int i = 0; i < 6000; i++)
+            {
+                byte[] sendBytes = Encoding.ASCII.GetBytes($"{name} {num} Hello");
+
+                client.Send(sendBytes, sendBytes.Length);
+                byte[] receiveBytes = client.Receive(ref endPoint);
+
+                string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                Console.WriteLine(receivedData);
+                num++;
+
+                Thread.Sleep(100);
+
+            }
+        }
+    }
+}
